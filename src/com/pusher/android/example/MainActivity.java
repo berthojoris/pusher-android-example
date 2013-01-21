d2f91611e9084131ce3735732e76f88de3158cb7
leggetter <phil@leggetter.co.uk>
2013-01-21 21:59:04 +0000

    added logging output to view

--- src/com/pusher/android/example/MainActivity.java
+++ src/com/pusher/android/example/MainActivity.java
@@ -7,6 +7,7 @@ import com.pusher.client.connection.ConnectionEventListener;
 import com.pusher.client.connection.ConnectionState;
 import com.pusher.client.connection.ConnectionStateChange;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.View;
@@ -14,6 +15,7 @@ import android.view.View.OnClickListener;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.Switch;
+import android.widget.TextView;
 
 public class MainActivity extends Activity implements ConnectionEventListener, ChannelEventListener {
 	
@@ -21,18 +23,22 @@ public class MainActivity extends Activity implements ConnectionEventListener, C
 	private Channel channel = null;
 	
 	private Switch connectionSwitch;
+	private TextView logTextView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		
 		pusher = new Pusher("8817c5eeccfb1ea2d1c6");
 		pusher.getConnection().bind(ConnectionState.ALL, this);
 		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+		logTextView = (TextView)this.findViewById(R.id.loggerText);
+		
+		// Connect/disconnect depending on switch state
 		connectionSwitch = (Switch)this.findViewById(R.id.connectSwitch);
 		connectionSwitch.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-
 			@Override
 			public void onCheckedChanged(CompoundButton button, boolean checked) {
 				if(checked) {
@@ -43,31 +49,67 @@ public class MainActivity extends Activity implements ConnectionEventListener, C
 				}				
 			}
 		});
+		
+		log("Application running");
 	}
 
 	@Override
-	public void onEvent(String arg0, String arg1, String arg2) {
-		// TODO Auto-generated method stub
-		
+	public void onEvent(String channelName, String eventName, String data) {
+		String msg = String.format("Event received: [%s] [%s] [%s]", channelName, eventName, data);
+		log( msg );
 	}
 
 	@Override
-	public void onSubscriptionSucceeded(String arg0) {
-		// TODO Auto-generated method stub
-		
+	public void onSubscriptionSucceeded(String channelName) {
+		String msg = String.format("Subscription succeeded for [%s]", channelName);
+		log( msg );
 	}
 
 	@Override
 	public void onConnectionStateChange(ConnectionStateChange change) {
-		System.out.println(
-				String.format("Connection state changed from [%s] to [%s]",
-						change.getPreviousState(), change.getCurrentState() ) );
+		String msg = String.format("Connection state changed from [%s] to [%s]",
+				change.getPreviousState(), change.getCurrentState() );
 		
+		log( msg );
 	}
 
 	@Override
-	public void onError(String arg0, String arg1, Exception arg2) {
-		// TODO Auto-generated method stub
+	public void onError(String message, String code, Exception e) {
+		String msg = String.format("Connection error: [%s] [%s] [%s]", message, code, e);
+		log(msg);
+	}
+	
+	private void log(String msg) {
+		LogTask task = new LogTask(logTextView, msg);
+		task.execute();
 	}
 
+}
+
+class LogTask extends AsyncTask<Void, Void, Void> {
+	
+	TextView view;
+	String msg;
+	
+	public LogTask(TextView view, String msg) {
+		this.view = view;
+		this.msg = msg;
+	}
+
+	@Override
+	protected Void doInBackground(Void... args) {
+		return null;
+	}
+
+	@Override
+	protected void onPostExecute(Void result) {
+		System.out.println(msg);
+		
+		String currentLog = view.getText().toString();
+		String newLog = msg + "\n" + currentLog;
+		view.setText(newLog);
+		
+		super.onPostExecute(result);
+	}
+	
 }

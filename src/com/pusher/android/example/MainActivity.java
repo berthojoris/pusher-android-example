b9406965d6e2e18edceef741a30cd26dca656e8d
Phil Leggetter <phil@leggetter.co.uk>
2013-02-13 23:09:17 +0000

    Using an encrypted connection

--- src/com/pusher/android/example/MainActivity.java
+++ src/com/pusher/android/example/MainActivity.java
@@ -1,42 +1,60 @@
 package com.pusher.android.example;
 
-import com.pusher.client.Pusher;
-import com.pusher.client.channel.Channel;
-import com.pusher.client.channel.ChannelEventListener;
-import com.pusher.client.connection.ConnectionEventListener;
-import com.pusher.client.connection.ConnectionState;
-import com.pusher.client.connection.ConnectionStateChange;
-
+import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.Switch;
 import android.widget.TextView;
 
-public class MainActivity extends Activity implements ConnectionEventListener, ChannelEventListener {
+import com.pusher.client.Pusher;
+import com.pusher.client.PusherOptions;
+import com.pusher.client.channel.Channel;
+import com.pusher.client.channel.ChannelEventListener;
+import com.pusher.client.connection.ConnectionEventListener;
+import com.pusher.client.connection.ConnectionState;
+import com.pusher.client.connection.ConnectionStateChange;
+import com.pusher.client.util.HttpAuthorizer;
+
+public class MainActivity extends Activity
+	implements ConnectionEventListener, ChannelEventListener {
+	
+	private static final String PUBLIC_CHANNEL_NAME = "a_channel";
 	
 	private Pusher pusher;
-	private Channel channel = null;
+	private Channel publicChannel;
 	
 	private Switch connectionSwitch;
 	private TextView logTextView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		// TODO: login token?
+		HttpAuthorizer authorizer = new HttpAuthorizer("http://www.leggetter.co.uk/pusher/pusher-examples/php/authentication/src/private_auth.php");
+		PusherOptions options = new PusherOptions().setEncrypted(true).setAuthorizer(authorizer);
+		pusher = new Pusher("8817c5eeccfb1ea2d1c6", options);
 		
-		pusher = new Pusher("8817c5eeccfb1ea2d1c6");
+		// bind to all connection events
 		pusher.getConnection().bind(ConnectionState.ALL, this);
 		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+		// Get view for logging
 		logTextView = (TextView)this.findViewById(R.id.loggerText);
 		
-		// Connect/disconnect depending on switch state
+		bindToConnectionSwitch();
+		
+		log("Application running");
+	}
+	
+	private void subscribeToPublicChannel() {
+		publicChannel = pusher.subscribe(PUBLIC_CHANNEL_NAME, this, "some_event");
+	}
+	
+	// Connect/disconnect depending on switch state
+	private void bindToConnectionSwitch() {
 		connectionSwitch = (Switch)this.findViewById(R.id.connectSwitch);
 		connectionSwitch.setOnCheckedChangeListener(new OnCheckedChangeListener() {
 			@Override
@@ -49,10 +67,30 @@ public class MainActivity extends Activity implements ConnectionEventListener, C
 				}				
 			}
 		});
+	}
+	
+	//ConnectionEventListener implementation
+	@Override
+	public void onConnectionStateChange(ConnectionStateChange change) {
+		String msg = String.format("Connection state changed from [%s] to [%s]",
+				change.getPreviousState(), change.getCurrentState() );
 		
-		log("Application running");
+		log( msg );
+		
+		if(change.getCurrentState() == ConnectionState.CONNECTED ) {
+			// TODO: when "auto-reconnection" and "allow subscriptions prior to connections.subscription"
+			// is implemented we should only need to subscribe once.
+			subscribeToPublicChannel();
+		}
 	}
 
+	@Override
+	public void onError(String message, String code, Exception e) {
+		String msg = String.format("Connection error: [%s] [%s] [%s]", message, code, e);
+		log(msg);
+	}
+
+	// ChannelEventListener implementation
 	@Override
 	public void onEvent(String channelName, String eventName, String data) {
 		String msg = String.format("Event received: [%s] [%s] [%s]", channelName, eventName, data);
@@ -64,21 +102,8 @@ public class MainActivity extends Activity implements ConnectionEventListener, C
 		String msg = String.format("Subscription succeeded for [%s]", channelName);
 		log( msg );
 	}
-
-	@Override
-	public void onConnectionStateChange(ConnectionStateChange change) {
-		String msg = String.format("Connection state changed from [%s] to [%s]",
-				change.getPreviousState(), change.getCurrentState() );
-		
-		log( msg );
-	}
-
-	@Override
-	public void onError(String message, String code, Exception e) {
-		String msg = String.format("Connection error: [%s] [%s] [%s]", message, code, e);
-		log(msg);
-	}
 	
+	// Logging helper method
 	private void log(String msg) {
 		LogTask task = new LogTask(logTextView, msg);
 		task.execute();
@@ -86,6 +111,7 @@ public class MainActivity extends Activity implements ConnectionEventListener, C
 
 }
 
+// Used for logging on the UI thread
 class LogTask extends AsyncTask<Void, Void, Void> {
 	
 	TextView view;
